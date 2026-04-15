366f6affcfb99d88e0e6ee3cec6836d8fa7880d8
ABHAY <abhay11082005@gmail.com>
2026-04-15 22:59:56 +0530

    changes

--- Day22/assignment3/assignment3/settings.py
+++ Day22/assignment3/assignment3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0jr!5!joi45-i4xqo^tq&r00w(4l8vr!n^)1ut+t!u7mzam*&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&0jr!5!joi45-i4xqo^tq&r00w(4l8vr!n^)1ut+t!u7mzam*&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
