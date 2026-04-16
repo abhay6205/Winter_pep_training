c5a98e7427fde661b9eb0b6fde89d01467415bae
ABHAY <abhay11082005@gmail.com>
2026-04-16 23:43:49 +0530

    file

--- Day18/todo_site/todo_site/settings.py
+++ Day18/todo_site/todo_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njdh59rm6_09q4_*j3((x7@r58xkn_c9cyq!q!tqbz@c0o=d28'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-njdh59rm6_09q4_*j3((x7@r58xkn_c9cyq!q!tqbz@c0o=d28')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
