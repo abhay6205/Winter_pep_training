1c6713c53951eadb0856f392a2ce3cc48a6b6fa9
ABHAY <abhay11082005@gmail.com>
2026-04-16 23:47:25 +0530

    file

--- Day14/main/main/settings.py
+++ Day14/main/main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)p*ruoq+66#py(tg#pm^gg_h(7awv6j9da4fk58og1dc9k5r70'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)p*ruoq+66#py(tg#pm^gg_h(7awv6j9da4fk58og1dc9k5r70')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
