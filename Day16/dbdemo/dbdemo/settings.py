6c55116f46301ae98e81f1350a04ed004d2c7e1a
ABHAY <abhay11082005@gmail.com>
2026-04-16 23:45:48 +0530

    file

--- Day16/dbdemo/dbdemo/settings.py
+++ Day16/dbdemo/dbdemo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7vaz*%n7w^z46zba4glfbh8lyar73mw#)sb#67c%gor+lwcc24'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-7vaz*%n7w^z46zba4glfbh8lyar73mw#)sb#67c%gor+lwcc24')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
